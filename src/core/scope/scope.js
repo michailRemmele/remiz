8b5a02f1d8cdc85b1dcf6de209991146059fbaec
mremmele <mikhail.remmele@7bits.it>
2018-09-08 22:55:39 +0600

    add on scope change actions support for scope provider and add error checking for scope methods

--- src/core/scope/scope.js
+++ src/core/scope/scope.js
@@ -1,13 +1,25 @@
+import ResolveDependencyStrategy from 'core/ioc/resolveDependencyStrategy';
+
 class Scope {
   constructor() {
     this.storage = {};
   }
 
   resolve(key, args) {
+    if (!this.storage[key]) {
+      throw new Error(`Resolution of dependency failed for key: ${key}`);
+    }
+
     return this.storage[key].resolve(args);
   }
 
   register(key, resolveDependencyStrategy) {
+    if (!(resolveDependencyStrategy instanceof ResolveDependencyStrategy)) {
+      throw new Error(
+        'Resolve dependency strategy should be inherits from ResolveDependencyStrategy'
+      );
+    }
+
     this.storage[key] = resolveDependencyStrategy;
   }
 
