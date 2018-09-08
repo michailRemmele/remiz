33f5db2cf59af281166fc83e688f2bb237a9cfcb
mremmele <mikhail.remmele@7bits.it>
2018-09-08 22:57:45 +0600

    add consts for ioc dependency resolution

---
+++ src/consts/global.js
@@ -0,0 +1,4 @@
+export const GENERAL_SCOPE_NAME = 'general';
+export const SCENE_PROVIDER_KEY_NAME = 'sceneProvider';
+export const ACTION_RESOLVER_KEY_NAME = 'actionResolver';
+export const KEY_RESOLVER_KEY_NAME = 'keyResolver';
