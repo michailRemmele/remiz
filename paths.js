25f39d6d5a980af1b2f783731c343ed731b8a710
mremmele <mikhail.remmele@7bits.it>
2018-09-08 22:37:28 +0600

    update webpack prod and dev configurations. Set mode to none, add resolve section to allow search modules at src folder and add alias for resources directory

--- paths.js
+++ paths.js
@@ -3,10 +3,11 @@
 const path = require('path');
 
 module.exports = {
-    build: path.join(__dirname, 'build'),
-    public: path.join(__dirname, 'public'),
-    indexHtml: path.join(__dirname, 'public/index.html'),
-    indexJs: path.join(__dirname, 'src/index.js'),
-    src: path.join(__dirname, 'src'),
-    nodeModules: path.join(__dirname, 'node_modules')
+  build: path.join(__dirname, 'build'),
+  public: path.join(__dirname, 'public'),
+  indexHtml: path.join(__dirname, 'public/index.html'),
+  indexJs: path.join(__dirname, 'src/index.js'),
+  src: path.join(__dirname, 'src'),
+  resources: path.join(__dirname, 'resources'),
+  nodeModules: path.join(__dirname, 'node_modules'),
 };
