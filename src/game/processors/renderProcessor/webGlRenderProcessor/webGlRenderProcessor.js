085e45190deb6a47eb4846f4860d9ea0e38e5c9d
mremmele <mikhail.remmele@7bits.it>
2019-02-07 22:58:34 +0600

    add texture rendering

--- src/game/processors/renderProcessor/webGlRenderProcessor/webGlRenderProcessor.js
+++ src/game/processors/renderProcessor/webGlRenderProcessor/webGlRenderProcessor.js
@@ -5,8 +5,8 @@ import terrainPlatform from 'resources/graphics/terrain/platform.png';
 
 class WebGlRenderProcessor {
   constructor() {
-    const window = document.getElementById('root');
-    this.gl = this.initGraphicContext(window);
+    const canvas = document.getElementById('root');
+    this.gl = this.initGraphicContext(canvas);
 
     this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
     this.gl.enable(this.gl.DEPTH_TEST);
@@ -14,6 +14,9 @@ class WebGlRenderProcessor {
     this.gl.clear(
       this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT
     );
+
+    webglUtils.resizeCanvasToDisplaySize(canvas, window.devicePixelRatio);
+    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
 
     this.graphicInit = false;
     this.program = this.initShaders();
@@ -25,11 +28,11 @@ class WebGlRenderProcessor {
       });
   }
 
-  initGraphicContext(window) {
+  initGraphicContext(canvas) {
     let graphicContext = null;
 
     try {
-      graphicContext = window.getContext('webgl') || window.getContext('experimental-webgl');
+      graphicContext = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
     } catch (e) {
       console.log('Unable to get graphic context.');
     }
@@ -42,10 +45,10 @@ class WebGlRenderProcessor {
   }
 
   initShaders() {
-    const shaderCreator = new ShaderBuilder(this.gl);
+    const shaderBuilder = new ShaderBuilder(this.gl);
 
-    const vertexShader = shaderCreator.create(shaderCreator.VERTEX_SHADER);
-    const fragmentShader = shaderCreator.create(shaderCreator.FRAGMENT_SHADER);
+    const vertexShader = shaderBuilder.create(shaderBuilder.VERTEX_SHADER);
+    const fragmentShader = shaderBuilder.create(shaderBuilder.FRAGMENT_SHADER);
 
     const shaderProgram = this.gl.createProgram();
     this.gl.attachShader(shaderProgram, vertexShader);
@@ -71,45 +74,37 @@ class WebGlRenderProcessor {
   }
 
   initGraphics() {
-    const setRectangle = (gl, x, y, width, height) => {
-      const x1 = x;
-      const x2 = x + width;
-      const y1 = y;
-      const y2 = y + height;
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        x1, y1,
-        x2, y1,
-        x1, y2,
-        x1, y2,
-        x2, y1,
-        x2, y2,
-      ]), gl.STATIC_DRAW);
-    };
-
     const attribSetters = webglUtils.createAttributeSetters(this.gl, this.program);
 
     const attribs = {
-      a_position: { buffer: this.gl.createBuffer(), numComponents: 2 },
-      a_texCoord: { buffer: this.gl.createBuffer(), numComponents: 2 },
+      position: {
+        data: [
+          0.0,  0.0,
+          1.0,  0.0,
+          0.0,  1.0,
+          0.0,  1.0,
+          1.0,  0.0,
+          1.0,  1.0,
+        ],
+        numComponents: 2,
+      },
+      texCoord: {
+        data: [
+          0.0,  0.0,
+          1.0,  0.0,
+          0.0,  1.0,
+          0.0,  1.0,
+          1.0,  0.0,
+          1.0,  1.0,
+        ],
+        numComponents: 2,
+      },
     };
+    const bufferInfo = webglUtils.createBufferInfoFromArrays(this.gl, attribs);
 
     this.gl.useProgram(this.program);
 
-    webglUtils.setAttributes(attribSetters, attribs);
-
-    setRectangle(this.gl, 0, 0, this.image.width, this.image.height);
-    this.gl.bufferData(
-      this.gl.ARRAY_BUFFER,
-      new Float32Array([
-        0.0,  0.0,
-        1.0,  0.0,
-        0.0,  1.0,
-        0.0,  1.0,
-        1.0,  0.0,
-        1.0,  1.0,
-      ]),
-      this.gl.STATIC_DRAW
-    );
+    webglUtils.setBuffersAndAttributes(this.gl, attribSetters, bufferInfo);
 
     const texture = this.gl.createTexture();
     this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
