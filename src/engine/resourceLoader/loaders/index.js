7fe384390c3d40129ed739d11bb8322936289000
mremmele <mikhail.remmele@7bits.it>
2019-03-26 11:28:38 +0600

    removed useless scrpit recources and the loader for it

--- src/engine/resourceLoader/loaders/index.js
+++ src/engine/resourceLoader/loaders/index.js
@@ -1,9 +1,7 @@
 import ImageLoader from './imageLoader';
 import JsonLoader from './jsonLoader';
-import ScriptLoader from './scriptLoader';
 
 export default {
   imageLoader: ImageLoader,
   jsonLoader: JsonLoader,
-  scriptLoader: ScriptLoader,
 };
