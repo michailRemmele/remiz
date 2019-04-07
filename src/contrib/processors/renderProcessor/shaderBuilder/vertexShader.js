e3b96e24173ce282e0292bfb46f893355499c40c
mremmele <mikhail.remmele@7bits.it>
2019-04-07 10:30:38 +0600

    added matrix transformer for rendering texture transformation. Added rotation, flipX and flipY transformation options for textures

--- src/contrib/processors/renderProcessor/shaderBuilder/vertexShader.js
+++ src/contrib/processors/renderProcessor/shaderBuilder/vertexShader.js
@@ -2,9 +2,7 @@ const vertexShader = `
 attribute vec2 a_position;
 attribute vec2 a_texCoord;
 
-uniform vec2 u_resolution;
-uniform vec2 u_scale;
-uniform vec2 u_translation;
+uniform mat3 u_matrix;
 
 uniform vec2 u_textureAtlasSize;
 uniform vec2 u_texCoordTranslation;
@@ -12,11 +10,7 @@ uniform vec2 u_texCoordTranslation;
 varying vec2 v_texCoord;
 
 void main() {
-  vec2 position = a_position + u_translation;
-  vec2 scaledPosition = u_scale * position;
-  vec2 convertedPosition = (scaledPosition.xy / u_resolution) * 2.0 - 1.0;
-
-  gl_Position = vec4(convertedPosition * vec2(1, -1), 0, 1);
+  gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0, 1);
   
   vec2 texCoord = a_texCoord + u_texCoordTranslation;
   v_texCoord = texCoord / u_textureAtlasSize;
