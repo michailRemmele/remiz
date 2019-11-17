e833227bffe212ff111909e1a5b92e1f62c6d1eb
Mikhail Remmele <mikhail.remmele@gmail.com>
2019-11-17 17:48:48 +0600

    added platform size meter processor

--- src/game/processorsPlugins/index.js
+++ src/game/processorsPlugins/index.js
@@ -3,6 +3,7 @@ import DamageProcessorPlugin from './damageProcessorPlugin/damageProcessorPlugin
 import EffectsProcessorPlugin from './effectsProcessorPlugin/effectsProcessorPlugin';
 import FallProcessorPlugin from './fallProcessorPlugin/fallProcessorPlugin';
 import GameOverProcessorPlugin from './gameOverProcessorPlugin/gameOverProcessorPlugin';
+import PlatformSizeMeterPlugin from './platformSizeMeterPlugin/platformSizeMeterPlugin';
 import ShootingProcessorPlugin from './shootingProcessorPlugin/shootingProcessorPlugin';
 import ReaperPlugin from './reaperPlugin/reaperPlugin';
 
@@ -12,6 +13,7 @@ export default {
   effectsProcessor: EffectsProcessorPlugin,
   fallProcessor: FallProcessorPlugin,
   gameOverProcessor: GameOverProcessorPlugin,
+  platformSizeMeter: PlatformSizeMeterPlugin,
   shootingProcessor: ShootingProcessorPlugin,
   reaper: ReaperPlugin,
 };
