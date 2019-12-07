4c9f8a955b8d34ea66e7b2a91bb8d49060a0de3d
Mikhail Remmele <mikhail.remmele@gmail.com>
2019-12-07 17:31:42 +0600

    wip: added easy ai strategy

--- src/game/processors/aiProcessor/aiStrategies/easyAIStrategy.js
+++ src/game/processors/aiProcessor/aiStrategies/easyAIStrategy.js
@@ -1,7 +1,11 @@
 import AIStrategy from './aiStrategy';
 
+const SHOT_MSG = 'SHOT';
+
 const PLATFORM_SIZE_NAME = 'platformSize';
 const PLAYERS_ENEMIES_NAME = 'playersEnemies';
+
+const TRANSFORM_COMPONENT_NAME = 'transform';
 
 class EasyAIStrategy extends AIStrategy{
   constructor(player, store) {
@@ -16,6 +20,20 @@ class EasyAIStrategy extends AIStrategy{
   update(messageBus, deltaTime) {
     const platformSize = this._store.get(PLATFORM_SIZE_NAME);
     const playerEnemies = this._store.get(PLAYERS_ENEMIES_NAME)[this._playerId];
+
+    if (!playerEnemies.length) {
+      return;
+    }
+
+    const enemy = playerEnemies[Math.floor(Math.random() * playerEnemies.length)];
+    const { offsetX: enemyX, offsetY: enemyY } = enemy.getComponent(TRANSFORM_COMPONENT_NAME);
+
+    messageBus.send({
+      gameObject: this._player,
+      type: SHOT_MSG,
+      x: enemyX,
+      y: enemyY,
+    });
   }
 }
 
