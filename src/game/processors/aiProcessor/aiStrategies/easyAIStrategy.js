ed6a22093a51ac53d302f942e906579a2fc47e67
Mikhail Remmele <mikhail.remmele@gmail.com>
2019-12-09 23:26:04 +0600

    wip: added ai enemies movement

--- src/game/processors/aiProcessor/aiStrategies/easyAIStrategy.js
+++ src/game/processors/aiProcessor/aiStrategies/easyAIStrategy.js
@@ -1,6 +1,7 @@
 import AIStrategy from './aiStrategy';
 
 const SHOT_MSG = 'SHOT';
+const MOVEMENT_MSG = 'MOVEMENT';
 
 const PLATFORM_SIZE_NAME = 'platformSize';
 const PLAYERS_ENEMIES_NAME = 'playersEnemies';
@@ -18,8 +19,7 @@ class EasyAIStrategy extends AIStrategy{
     this._playerId = this._player.getId();
   }
 
-  update(messageBus, deltaTime) {
-    const platformSize = this._store.get(PLATFORM_SIZE_NAME);
+  _attack(messageBus) {
     const playerEnemies = this._store.get(PLAYERS_ENEMIES_NAME)[this._playerId];
     const weapon = this._player.getComponent(WEAPON_COMPONENT_NAME);
 
@@ -37,6 +37,49 @@ class EasyAIStrategy extends AIStrategy{
       x: enemyX,
       y: enemyY,
     });
+  }
+
+  _getAngleBetweenTwoPoints(x1, x2, y1, y2) {
+    return Math.atan2(y1 - y2, x1 - x2);
+  }
+
+  _radToDeg(rad) {
+    const angleInDegrees = rad * 180 / Math.PI;
+    return angleInDegrees < 0 ? angleInDegrees + 360 : angleInDegrees;
+  }
+
+  _move(messageBus) {
+    const playerEnemies = this._store.get(PLAYERS_ENEMIES_NAME)[this._playerId];
+    const mainEnemy = playerEnemies.find((enemy) => {
+      return enemy.getId() === '1';
+    });
+
+    if (!mainEnemy) {
+      return;
+    }
+
+    const mainEnemyPosition = mainEnemy.getComponent(TRANSFORM_COMPONENT_NAME);
+    const playerPosition = this._player.getComponent(TRANSFORM_COMPONENT_NAME);
+    const movementAngle = this._radToDeg(this._getAngleBetweenTwoPoints(
+      mainEnemyPosition.offsetX,
+      playerPosition.offsetX,
+      mainEnemyPosition.offsetY,
+      playerPosition.offsetY
+    ));
+
+    messageBus.send({
+      type: MOVEMENT_MSG,
+      gameObject: this._player,
+      id: this._player.getId(),
+      directionAngle: movementAngle,
+    });
+  }
+
+  update(messageBus, deltaTime) {
+    const platformSize = this._store.get(PLATFORM_SIZE_NAME);
+
+    this._attack(messageBus);
+    this._move(messageBus);
   }
 }
 
