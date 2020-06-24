5816dda64cd5d6e6fae5f637b1550a6b291f054a
Mikhail Remmele <mikhail.remmele@gmail.com>
2020-06-24 23:06:42 +0600

    added mtvs support for circles collisions

--- src/contrib/processors/collisionDetectionProcessor/intersectionCheckers/circlesIntersectionChecker.js
+++ src/contrib/processors/collisionDetectionProcessor/intersectionCheckers/circlesIntersectionChecker.js
@@ -1,21 +1,41 @@
+import { Vector2 } from 'engine/mathLib';
+
 import IntersectionChecker from './intersectionChecker';
-import SAT from './sat';
 
 class CirclesIntersectionChecker extends IntersectionChecker {
-  constructor() {
-    super();
-    this.sat = new SAT();
-  }
-
   check(arg1, arg2) {
     const { radius: rArg1 } = arg1.collider;
     const { radius: rArg2 } = arg2.collider;
     const { x: xArg1, y: yArg1 } = arg1.coordinates.center;
     const { x: xArg2, y: yArg2 } = arg2.coordinates.center;
 
-    const distance = Math.sqrt(Math.pow(xArg1 - xArg2, 2) + Math.pow(yArg1 - yArg2, 2));
+    const x = xArg1 - xArg2;
+    const y = yArg1 - yArg2;
+    const distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 
-    return distance < rArg1 + rArg2;
+    if (distance >= rArg1 + rArg2) {
+      return false;
+    }
+
+    const mtv = new Vector2(x, y);
+    const overlap = rArg1 + rArg2 - distance;
+    mtv.multiplyNumber(1 / distance * overlap);
+
+    const positiveX = Math.abs(mtv.x);
+    const negativeX = -Math.abs(mtv.x);
+    const positiveY = Math.abs(mtv.y);
+    const negativeY = -Math.abs(mtv.y);
+
+    return {
+      mtv1: new Vector2(
+        xArg1 < xArg2 ? negativeX : positiveX,
+        yArg1 < yArg2 ? negativeY : positiveY
+      ),
+      mtv2: new Vector2(
+        xArg2 < xArg1 ? negativeX : positiveX,
+        yArg2 < yArg1 ? negativeY : positiveY
+      ),
+    };
   }
 }
 
