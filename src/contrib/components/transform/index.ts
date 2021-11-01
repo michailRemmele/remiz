4e74bfac14b83b0be7438b7aafffc3a8fb5cb5bb
Mikhail Remmele <mikhail.remmele@gmail.com>
2021-11-01 18:52:33 +0300

    add additional methods for transform component to get relative values of game object position

--- src/contrib/components/transform/index.ts
+++ src/contrib/components/transform/index.ts
@@ -33,12 +33,28 @@ export class Transform extends Component {
     return parentComponent ? parentComponent[name] : defaultValue;
   }
 
+  set relativeOffsetX(offsetX) {
+    this._offsetX = offsetX;
+  }
+
+  get relativeOffsetX() {
+    return this._offsetX;
+  }
+
   set offsetX(offsetX) {
     this._offsetX = offsetX - this._getPropertyFromParent('offsetX', 0);
   }
 
   get offsetX() {
     return this._offsetX + this._getPropertyFromParent('offsetX', 0);
+  }
+
+  set relativeOffsetY(offsetY) {
+    this._offsetY = offsetY;
+  }
+
+  get relativeOffsetY() {
+    return this._offsetY;
   }
 
   set offsetY(offsetY) {
@@ -49,12 +65,28 @@ export class Transform extends Component {
     return this._offsetY + this._getPropertyFromParent('offsetY', 0);
   }
 
+  set relativeOffsetZ(offsetZ) {
+    this._offsetZ = offsetZ;
+  }
+
+  get relativeOffsetZ() {
+    return this._offsetZ;
+  }
+
   set offsetZ(offsetZ) {
     this._offsetZ = offsetZ - this._getPropertyFromParent('offsetZ', 0);
   }
 
   get offsetZ() {
     return this._offsetZ + this._getPropertyFromParent('offsetZ', 0);
+  }
+
+  set relativeRotation(rotation) {
+    this._rotation = rotation;
+  }
+
+  get relativeRotation() {
+    return this._rotation;
   }
 
   set rotation(rotation) {
@@ -65,12 +97,28 @@ export class Transform extends Component {
     return this._rotation + this._getPropertyFromParent('rotation', 0);
   }
 
+  set relativeScaleX(scaleX) {
+    this._scaleX = scaleX;
+  }
+
+  get relativeScaleX() {
+    return this._scaleX;
+  }
+
   set scaleX(scaleX) {
     this._scaleX = scaleX / this._getPropertyFromParent('scaleX', 1);
   }
 
   get scaleX() {
     return this._scaleX * this._getPropertyFromParent('scaleX', 1);
+  }
+
+  set relativeScaleY(scaleY) {
+    this._scaleY = scaleY;
+  }
+
+  get relativeScaleY() {
+    return this._scaleY;
   }
 
   set scaleY(scaleY) {
