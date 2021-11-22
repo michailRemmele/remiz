e532404e34072eb422afb724fa96545afa6d0cea
Mikhail Remmele <mikhail.remmele@gmail.com>
2021-11-22 21:50:27 +0300

    add more tests for message bus

--- src/engine/message-bus/tests/message-bus.test.ts
+++ src/engine/message-bus/tests/message-bus.test.ts
@@ -64,4 +64,130 @@ describe('Engine -> MessageBus', () => {
 
     expect(messageBus.get('test-message-1')).toEqual([message1, message1, message2]);
   });
+
+  it('Sends and returns messages with delay correctly', () => {
+    const messageBus = new MessageBus();
+
+    const message1 = {
+      type: 'test-message-1',
+      id: 'id-1',
+      arg1: 12,
+      arg: 'Hello world',
+    };
+    const message2 = {
+      type: 'test-message-2',
+      arg1: false,
+      arg: 100,
+    };
+
+    messageBus.send(message1, true);
+    messageBus.send(message2, true);
+
+    expect(messageBus.getMessageCount()).toEqual(0);
+
+    expect(messageBus.get('test-message-1')).toBeUndefined();
+    expect(messageBus.get('test-message-2')).toBeUndefined();
+
+    messageBus.sendDelayed();
+
+    expect(messageBus.getMessageCount()).toEqual(2);
+
+    expect(messageBus.get('test-message-1')).toEqual([message1]);
+    expect(messageBus.get('test-message-2')).toEqual([message2]);
+  });
+
+  it('Deletes messages correctly', () => {
+    const messageBus = new MessageBus();
+
+    const message1 = {
+      type: 'test-message-1',
+      id: 'id-1',
+      arg1: 12,
+      arg: 'Hello world',
+    };
+    const message2 = {
+      type: 'test-message-2',
+      arg1: false,
+      arg: 100,
+    };
+    const message3 = {
+      type: 'test-message-1',
+    };
+
+    messageBus.send(message1);
+    messageBus.send(message2);
+    messageBus.send(message3);
+
+    expect(messageBus.getMessageCount()).toEqual(3);
+
+    messageBus.delete('test-message-2');
+    messageBus.deleteById('test-message-1', 'id-1');
+
+    expect(messageBus.getMessageCount()).toEqual(1);
+
+    expect(messageBus.get('test-message-1')).toEqual([message3]);
+    expect(messageBus.get('test-message-2')).toBeUndefined();
+  });
+
+  it('Clears messages correctly', () => {
+    const messageBus = new MessageBus();
+
+    const message1 = {
+      type: 'test-message-1',
+      id: 'id-1',
+      arg1: 12,
+    };
+    const message2 = {
+      type: 'test-message-2',
+      arg1: false,
+    };
+
+    messageBus.send(message1);
+    messageBus.send(message2);
+    messageBus.send(message2, true);
+
+    expect(messageBus.getMessageCount()).toEqual(2);
+
+    messageBus.clear();
+
+    expect(messageBus.getMessageCount()).toEqual(0);
+
+    expect(messageBus.get('test-message-1')).toBeUndefined();
+    expect(messageBus.get('test-message-2')).toBeUndefined();
+
+    messageBus.sendDelayed();
+
+    expect(messageBus.get('test-message-2')).toEqual([message2]);
+  });
+
+  it('Resets messages correctly', () => {
+    const messageBus = new MessageBus();
+
+    const message1 = {
+      type: 'test-message-1',
+      id: 'id-1',
+      arg1: 12,
+    };
+    const message2 = {
+      type: 'test-message-2',
+      arg1: false,
+    };
+
+    messageBus.send(message1);
+    messageBus.send(message2);
+    messageBus.send(message2, true);
+
+    expect(messageBus.getMessageCount()).toEqual(2);
+
+    messageBus.reset();
+
+    expect(messageBus.getMessageCount()).toEqual(0);
+
+    expect(messageBus.get('test-message-1')).toBeUndefined();
+    expect(messageBus.get('test-message-2')).toBeUndefined();
+
+    messageBus.sendDelayed();
+
+    expect(messageBus.get('test-message-2')).toBeUndefined();
+  });
 });
