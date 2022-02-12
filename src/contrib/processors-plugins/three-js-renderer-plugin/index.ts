11521d314cc8da82ac3b06fd24fc8c71a49047e2
Mikhail Remmele <mikhail.remmele@gmail.com>
2022-02-12 21:02:38 +0300

    add runtime sprite crop to load whole sprite at once instead of loading each frame separetely

--- src/contrib/processors-plugins/three-js-renderer-plugin/index.ts
+++ src/contrib/processors-plugins/three-js-renderer-plugin/index.ts
@@ -5,10 +5,15 @@ import type { PrefabCollection } from '../../../engine/prefab';
 import type { Renderable } from '../../components/renderable';
 import IOC from '../../../engine/ioc/ioc';
 import { ThreeJSRenderer } from '../../processors/three-js-renderer';
-import { PREFAB_COLLECTION_KEY_NAME } from '../../../engine/consts/global';
+import { PREFAB_COLLECTION_KEY_NAME, RESOURCES_LOADER_KEY_NAME } from '../../../engine/consts/global';
 
 const RENDERABLE_COMPONENT_NAME = 'renderable';
 const TRANSFORM_COMPONENT_NAME = 'transform';
+
+// TODO: Remove once resource loader will be moved to ts
+interface ResourceLoader {
+  load: (resource: string) => Promise<HTMLImageElement>;
+}
 
 interface ThreeJSRendererPluginOptions extends ProcessorPluginOptions {
   windowNodeId: string
@@ -35,6 +40,7 @@ export class ThreeJSRendererPlugin implements ProcessorPlugin {
     }
 
     const textureLoader = new TextureLoader();
+    const resourceLoader = IOC.resolve(RESOURCES_LOADER_KEY_NAME) as ResourceLoader;
 
     const prefabCollection = IOC.resolve(PREFAB_COLLECTION_KEY_NAME) as PrefabCollection;
     const gameObjectObserver = createGameObjectObserver({
@@ -71,20 +77,54 @@ export class ThreeJSRendererPlugin implements ProcessorPlugin {
 
     const textureMap: Record<string, Array<Texture>> = {};
 
+    const spriteCropper = document.createElement('canvas');
+    const spriteCropperCtx = spriteCropper.getContext('2d');
+
+    if (!spriteCropperCtx) {
+      throw new Error('Can\'t create canvas context for sprite cropper');
+    }
+
     await Promise.all(
       Object.keys(imagesToLoad).map((key) => {
         const renderable = imagesToLoad[key];
 
-        const frames = renderable.slice || 1;
-
-        const promises: Array<Promise<Texture>> = [];
-        for (let i = 0; i < frames; i += 1) {
-          promises.push(textureLoader.loadAsync(`${renderable.src}/${i}.png`));
+        if (renderable.type === 'static') {
+          return textureLoader.loadAsync(renderable.src)
+            .then((texture) => { textureMap[key] = [texture]; });
         }
 
-        return Promise.all(promises)
-          .then((textures) => { textureMap[key] = textures; })
-          .catch(() => {});
+        return resourceLoader.load(renderable.src)
+          .then((spriteImage) => {
+            const frames = renderable.slice as number;
+            const frameWidth = spriteImage.width / frames;
+            const frameHeight = spriteImage.height;
+
+            spriteCropper.width = frameWidth;
+            spriteCropper.height = frameHeight;
+
+            textureMap[key] = new Array<Texture>(frames);
+
+            for (let i = 0; i < frames; i += 1) {
+              spriteCropperCtx.clearRect(0, 0, frameWidth, frameHeight);
+
+              spriteCropperCtx.drawImage(
+                spriteImage,
+                frameWidth * i, 0,
+                frameWidth, frameHeight,
+                0, 0,
+                frameWidth, frameHeight,
+              );
+
+              const frameImageData = spriteCropperCtx.getImageData(0, 0, frameWidth, frameHeight);
+
+              // Some issue with three.js d.ts probably,
+              // cause it should take ImageData as first argument
+              const texture = new Texture(frameImageData as unknown as HTMLImageElement);
+              texture.needsUpdate = true;
+
+              textureMap[key][i] = texture;
+            }
+          });
       }),
     );
 
