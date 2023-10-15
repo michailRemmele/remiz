8bef7465ae24a1fd2271a9dccd898e4f2563ec35
Mikhail Remmele <mikhail.remmele@gmail.com>
2023-10-15 11:55:47 +0300

    remove unused mocks

--- src/__mocks__/component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '../engine/component';
-
-export const createMockComponent = (name: string, config?: Record<string, unknown>): Component => ({
-  clone: () => createMockComponent(name, config),
-  getParentComponent: (): void => {},
-  gameObject: void 0,
-  ...config,
-});
